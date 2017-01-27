d5f9bc0002e62bdbac5d025fe8888189e05b31a2
Krystyna Malewski <krmalewski@gmail.com>
2017-01-27 17:36:02 -0500

    update user id

--- env-vars.js
+++ env-vars.js
@@ -1,5 +1,5 @@
 module.exports = {
   'SLACKBOT_KEY':'xoxb-124950273607-TlAPeE7m4CyHO0zAc3mqfDuc',
-  'TA_ID':'U16DJCTJT',
-  'ADMIN_ID':'U16DJCTJT'
+  'TA_ID':'U2CKNM7UN',
+  'ADMIN_ID':'U2CKNM7UN'
 };
